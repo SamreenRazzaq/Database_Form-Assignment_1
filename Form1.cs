b456a27690eb456a362944df2eddb513d9840b01
Samreen Razzaq <96229701+SamreenRazzaq@users.noreply.github.com>
2023-10-08 17:08:48 +0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Data.SqlClient;
+
+namespace DBTheoryTask
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Form1_Load() called...");
+            txtMessageText.Text = "Startup..."; try
+            {
+                System.Diagnostics.Debug.WriteLine("within the try");
+                SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-546PB2I\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+                connection.Open();
+                txtMessageText.Text = "Connection Successful";
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                txtMessageText.Text = "Error, " + ex;
+            }
+        }
+        private void BtnView_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand();
+            SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-PH51JNI\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True;TrustServerCertificate=True");
+            connection.Open();
+            txtMessageText.Text = "Retrieving Records...";
+            command.Connection = connection;
+            command.CommandText = "select * from Customers";
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            txtMessageText.Text = "Retrieval Successful!";
+            connection.Close();
+        }
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand();
+            SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-PH51JNI\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True;TrustServerCertificate=True");
+            connection.Open();
+            txtMessageText.Text = "Inserting Record...";
+            command.Connection = connection;
+            command.CommandText = "insert into Customers (CustomerID, CompanyName) values('" +
+           txtID.Text + "','" + txtCompanyName.Text + "')";
+            command.ExecuteNonQuery();
+            txtMessageText.Text = "Record Inserted...";
+            connection.Close();
+        }
+        private void BtnCount_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand();
+            SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-PH51JNI\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True;TrustServerCertificate=True");
+            connection.Open();
+            txtMessageText.Text = "Counting Records...";
+            command.Connection = connection;
+            command.CommandText = "select count(*) from Customers";
+            int count = (int)command.ExecuteScalar();
+            txtMessageText.Text = "Number of records: " + count;
+            connection.Close();
+        }
+    }
+}
